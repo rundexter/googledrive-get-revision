9788e4354cab786ecbb83f2abcb3637c32f8cd6d
Alexander Zaretskii <al.zaretskii@gmail.com>
2016-01-04 19:38:24 +0200

    add DEXTER module

--- index.js
+++ index.js
@@ -1,4 +1,51 @@
+var google = require('googleapis'),
+    util = require('./util.js');
+
+var service = google.drive('v3'),
+    reqFields = 'id,lastModifyingUser,mimeType,modifiedTime,originalFilename,size';
+
+var pickInputs = {
+        'fileId': { key: 'fileId', validate: { req: true } },
+        'revisionId': { key: 'revisionId', validate: { req: true } },
+        'acknowledgeAbuse': 'acknowledgeAbuse'
+    },
+    pickOutputs = {
+        'id': 'id',
+        'mimeType': 'mimeType',
+        'modifiedTime': 'modifiedTime',
+        'displayName': 'lastModifyingUser.displayName',
+        'emailAddress': 'lastModifyingUser.emailAddress',
+        'originalFilename': 'originalFilename',
+        'size': 'size'
+    };
+
 module.exports = {
+
+    /**
+     * Get auth data.
+     *
+     * @param step
+     * @param dexter
+     * @returns {*}
+     */
+    authOptions: function (step, dexter) {
+        var OAuth2 = google.auth.OAuth2,
+            oauth2Client = new OAuth2();
+
+        if(!dexter.environment('google_access_token')) {
+
+            this.fail('A [google_access_token] environment variable is required for this module');
+            return false;
+        } else {
+
+            oauth2Client.setCredentials({
+                access_token: dexter.environment('google_access_token')
+            });
+
+            return oauth2Client;
+        }
+    },
+
     /**
      * The main entry point for the Dexter module
      *
@@ -6,8 +53,26 @@ module.exports = {
      * @param {AppData} dexter Container for all data used in this workflow.
      */
     run: function(step, dexter) {
-        var results = { foo: 'bar' };
-        //Call this.complete with the module's output.  If there's an error, call this.fail(message) instead.
-        this.complete(results);
+        var auth = this.authOptions(step, dexter),
+            inputs = util.pickInputs(step, pickInputs),
+            validateErrors = util.checkValidateErrors(inputs, pickInputs);
+
+        if (!auth)
+            return;
+
+        if (validateErrors)
+            return this.fail(validateErrors);
+
+        inputs.fields = reqFields;
+
+        // set credential
+        google.options({ auth: auth });
+        service.revisions.get(inputs, function (error, data) {
+            console.log(data);
+            if (error)
+                this.fail(error);
+             else
+                this.complete(util.pickOutputs(data, pickOutputs));
+        }.bind(this));
     }
 };
